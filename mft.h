7c6417e695b5966ed519400af5e8357313d4e7a8
Unknown <ucty@diginex.cz>
2017-12-17 19:20:27 +0100

    komentare

--- mft.h
+++ mft.h
@@ -2,12 +2,18 @@
 #define MFT_H
 
 #define MFT_FRAG_COUNT = 32;
+#define UID_ITEM_FREE = 0;
 
+// item muze mit 1 az X techto fragmentu
+// +---+---+---+---+---+---+---+---+
+// | 1 | 2 | X | X | X | 3 | 4 | 5 |
+// +---+---+---+---+---+---+---+---+
 struct mft_fragment {
     int32_t fragment_start_address;     //start adresa
     int32_t fragment_count;             //pocet clusteru ve fragmentu
 };
 
+// jeden soubor muze byt slozen i z vice mft_itemu, ty ale pak maji stejna uid (lisi se item_orderem)
 struct mft_item {
     int32_t uid;                                        //UID polozky, pokud UID = UID_ITEM_FREE, je polozka volna
     int isDirectory;                                    //soubor, nebo adresar (1=adresar, 0=soubor)
@@ -15,7 +21,7 @@ struct mft_item {
     int8_t item_order_total;                            //celkovy pocet polozek v MFT
     char item_name[12];                                 //8+3 + /0 C/C++ ukoncovaci string znak
     int32_t item_size;                                  //velikost souboru v bytech
-    struct mft_fragment fragments[32]; 			//fragmenty souboru - MFT fragments count
+    struct mft_fragment fragments[32]; 			        //fragmenty souboru - MFT fragments count
 };
 
 #endif
