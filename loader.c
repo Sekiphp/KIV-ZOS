dde1ed1ec7b1f5cdc6115cf8d6d6a5ba5ea1998a
Unknown <ucty@diginex.cz>
2017-12-17 21:13:51 +0100

    Automaticke zalozeni souboru

--- loader.c
+++ loader.c
@@ -18,26 +18,116 @@
 void * loader(void * arg){
     sdilenaPamet *param = (sdilenaPamet *) arg;
     FILE *fr;
+    struct boot_record *bootr;
+    int cluster_size, cluster_count;
 
     printf("LOADER starting...\n");
+    printf("\tZkousim otevrit soubor: %s\n", param->soubor);
 
-    printf("louda %s\n", param->soubor);
-
-
-
-//    boot = malloc(sizeof(struct boot_record));
+    bootr = malloc(sizeof(struct boot_record));
     fr = fopen(param->soubor, "rb");
     if (fr != NULL) {
-        fread(boot, sizeof(struct boot_record), 1, fr);
+        fread(bootr, sizeof(struct boot_record), 1, fr);
+
+        printf("boot loader: %s\n",boot->signature);
+
         fclose(fr);
     }
+    else {
+        printf("\tNepodarilo se otevrit soubor %s\n", param->soubor);
 
-    printf("boot loader: %s\n",boot->signature);
+        cluster_size = 1024;
+        cluster_count = 10;
+
+        printf("\tZakladam soubor %s\n", param->soubor);
+        printf("\t\tPocet clusteru je %d\n",cluster_count);
+        printf("\t\tVelikost clusteru je %d\n", cluster_size);
+
+        zaloz_soubor(cluster_size, cluster_count, param->soubor);
+    }
 
 
 
 
 
+
+    free((void *) bootr);
     printf("LOADER ending\n");
     return NULL;
+}
+
+/* Zalozi pseudoNFTS soubor obsahujici ROOT_DIR */
+/* int cluster_size = Velikost clusteru (default = 1024) */
+/* int clutser_count = Pocet clusteru (default = 10) */
+int zaloz_soubor(int cluster_size, int cluster_count, char filename[]){
+    FILE *file;
+    struct boot_record *bootr;
+    struct mft_item *mfti;
+    struct mft_fragment *mftf;
+    int i, bitmapa[cluster_count];
+
+    /* Provedeme si nejake (pomocne) vypocty, vse s dostatecnou rezervou */
+    int sirka_vsech_fragmentu = cluster_count * sizeof(struct mft_fragment); // pri spatnem scenari
+    int sirka_vsech_clusteru = cluster_count * sizeof(struct mft_item); // pri spatnem scenari
+    int sirka_bitmapy = 4 * cluster_count;
+
+    int bitmap_start = sizeof(struct boot_record) + sirka_vsech_fragmentu + sirka_vsech_clusteru;
+    int data_start = bitmap_start + sirka_bitmapy;
+
+
+    /* Zacneme zapisovat do souboru */
+    file = fopen(filename, "wb");
+    if(file != NULL){
+        /* Zapiseme boot record */
+        bootr = malloc(sizeof(struct boot_record));
+
+        strcpy(bootr->signature, "Hubacek");
+        strcpy(bootr->volume_descriptor, "pseudo NTFS 2017");
+        bootr->disk_size = cluster_size * cluster_count; // 10 * 1024
+        bootr->cluster_size = cluster_size; // 1024
+        bootr->cluster_count = cluster_count; // 10
+        bootr->mft_start_address = sizeof(struct boot_record); // 288b
+        bootr->bitmap_start_address = bitmap_start; // odrazim se od 288b
+        bootr->data_start_address = data_start; // 4b ma jedna polozka, polozek je jako cluster_pocet
+        bootr->mft_max_fragment_count = 1;
+
+        fwrite(bootr, sizeof(struct boot_record), 1, file);
+        free((void *) bootr);
+
+        /* Zapiseme startovaci bitmapu - vse volne krome 1 (ROOT_DIR) */
+        /* Posunu se na zacatek oblasti pro bitmapu */
+        fseek(file, bitmap_start, SEEK_SET);
+        bitmapa[0] = 1;
+        for (i = 1; i < cluster_count; i++){
+            bitmapa[i] = 0;
+        }
+        fwrite(bitmapa, sizeof(bitmapa[i]), cluster_count, file);
+
+        /* Zapiseme ROOT_DIR do MFT tabulky, ROOT_DIR bude vzdy prvni v MFT tabulce*/
+        /* ROOT_DIR se sklada z jdnoho itemu a jednoho fragmentu */
+        /* Posunu se na zacatek oblasti MFT */
+        fseek(file, sizeof(struct boot_record), SEEK_SET);
+
+        mfti = malloc(sizeof(struct mft_item));
+
+        mftf.fragment_start_address = data_start; // start adresa ve VFS
+        mftf.fragment_count = 1; // pocet clusteru ve VFS od startovaci adresy
+
+        mfti->uid = 1;
+        mfti->isDirectory = 1;
+        mfti->item_order = 1;
+        mfti->item_order_total = 1;
+        strcpy(mfti->item_name, "ROOT_DIR");
+        mfti->item_size = 0; // zatim tam nic neni, takze nula
+        mfti->fragments[0] = mftf;
+
+        fwrite(mfti, sizeof(struct mft_item), 1, file);
+        free((void *) mfti);
+
+        /* Tady bychom meli zapsat obceh ROOT_DIRU */
+        /* Jelikoz v nem ale pri prvnim spusteni nic neni, tak nic nezapisujeme :) */
+
+        fclose(file);
+    }
+
 }
--- loader.h
+++ loader.h
@@ -3,5 +3,6 @@
 
     /* Hlavicky funkci ze souboru loader.c -> komentare jsou tam */
     void *loader(void * arg);
+    int zaloz_soubor(int cluster_size, int cluster_count, char filename[]);
 
 #endif
