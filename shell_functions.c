6d591c4e0942a8fd9ed59e5db96666f32be816b7
Unknown <ucty@diginex.cz>
2018-01-17 18:39:10 +0100

    incp 1 fix

--- shell_functions.c
+++ shell_functions.c
@@ -169,7 +169,7 @@ void func_incp(char *cmd){
             if (f == NULL)
             {
                 *result = NULL;
-                return -1; // -1 means file opening fail
+                return; // -1 means file opening fail
             }
             fseek(f, 0, SEEK_END);
             size = ftell(f);
@@ -179,7 +179,7 @@ void func_incp(char *cmd){
             if (size != fread(*result, sizeof(char), size, f))
             {
                 free(*result);
-                return -2; // -2 means file reading fail
+                return; // -2 means file reading fail
             }
             fclose(f);
             (*result)[size] = 0;
