685e4d09584183fd65ca5af015976891ab3cdc16
Unknown <ucty@diginex.cz>
2018-01-22 23:01:32 +0100

    improve debugger

--- debugger.h
+++ debugger.h
@@ -1,7 +1,8 @@
 #define DEBUG 1
 
 #if defined(DEBUG) && DEBUG > 0
-# define DEBUG_PRINT(x) printf x
+ #define DEBUG_PRINT(fmt, args...) fprintf(stderr, "DEBUG: %s:%d:%s(): " fmt, \
+    __FILE__, __LINE__, __func__, ##args)
 #else
-# define DEBUG_PRINT(x) do {} while (0)
+ #define DEBUG_PRINT(fmt, args...) /* Don't do anything in release builds */
 #endif
