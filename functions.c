23f7b1e08a24d8c1716da07dc34df1d3186a4603
Unknown <ucty@diginex.cz>
2018-02-01 00:59:18 +0100

    better comparsion of files

--- functions.c
+++ functions.c
@@ -69,11 +69,10 @@ int get_uid_by_name(char *dir_name, int uid_pwd){
             if (hledane < CLUSTER_COUNT && mft_seznam[hledane] != NULL){
                 mfti = mft_seznam[hledane]->item;
 
-                DEBUG_PRINT("\tHledane mfti s uid=%d (%s ?= %s), dir_len=%d, NOT NULL\n", hledane, mfti.item_name, dir_name, strlen(mfti.item_name));
+                DEBUG_PRINT("\tHledane mfti s uid=%d (%s ?= %s) && (%d ?= %d), NOT NULL\n", hledane, mfti.item_name, dir_name, strlen(mfti.item_name), strlen(dir_name));
 
                 // todo - isDirectory ... nelze overit unikatnost jmena
-                // if (strncmp(mfti.item_name, dirname, cmp_len) == 0 && mfti.isDirectory == 1) {
-                if (strncmp(mfti.item_name, dir_name, strlen(mfti.item_name)) == 0) {
+                if (strncmp(mfti.item_name, dir_name, strlen(mfti.item_name)) == 0 && strlen(mfti.item_name) == strlen(dir_name)) {
                     DEBUG_PRINT("\t\tSHODA\n");
                     return mfti.uid;
                 }
