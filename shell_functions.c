4687ae5ddc4a326dd75ede7bff95d7e16ff1b342
Unknown <ucty@diginex.cz>
2018-02-01 01:22:06 +0100

    test fix

--- shell_functions.c
+++ shell_functions.c
@@ -751,8 +751,8 @@ void func_outcp(char *cmd){
 
     // part 2, ulozim soubor do pc
     DEBUG_PRINT("Ulozim soubor do pc\n");
-    cmd = strtok(NULL, " \n");
-    strncpy(pc_file, cmd, strlen(cmd));
+    cmd = strtok(NULL, " ");
+    strcpy(pc_file, cmd);
     //pc_file[strlen(cmd)] = '\0';
     DEBUG_PRINT("test=%s\n", pc_file);
 
